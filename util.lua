d598a2997df4cad070f117ae5259538624239426
FaceDeer <derksenmobile@gmail.com>
2019-01-02 02:43:21 -0700

    pull another table out of a function into file-level context

--- util.lua
+++ util.lua
@@ -17,14 +17,16 @@ digtron.find_new_pos = function(pos, facing)
 	return vector.add(pos, dir)
 end
 
+local facedir_to_down_dir_table = {
+	[0]={x=0, y=-1, z=0},
+	{x=0, y=0, z=-1},
+	{x=0, y=0, z=1},
+	{x=-1, y=0, z=0},
+	{x=1, y=0, z=0},
+	{x=0, y=1, z=0}
+}
 digtron.facedir_to_down_dir = function(facing)
-	return (
-		{[0]={x=0, y=-1, z=0},
-		{x=0, y=0, z=-1},
-		{x=0, y=0, z=1},
-		{x=-1, y=0, z=0},
-		{x=1, y=0, z=0},
-		{x=0, y=1, z=0}})[math.floor(facing/4)]
+	return facedir_to_down_dir_table[math.floor(facing/4)]
 end
 
 digtron.find_new_pos_downward = function(pos, facing)
