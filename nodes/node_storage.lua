321444b93675fbe46f1a51971067f525d3416e50
1F616EMO~nya <root@1f616emo.xyz>
2024-07-20 03:37:46 +0800

    Allow moving fuel items in combined storage by shift-clicking (#109)

--- nodes/node_storage.lua
+++ nodes/node_storage.lua
@@ -206,6 +206,8 @@ local combined_storage_formspec_string =
 	"list[current_name;fuel;0,4.1;8,1;]" ..
 	"list[current_player;main;0,5.75;8,1;]" ..
 	"list[current_player;main;0,6.98;8,3;8]" ..
+	"listring[current_name;fuel]" ..
+	"listring[current_player;main]" ..
 	"listring[current_name;main]" ..
 	"listring[current_player;main]" ..
 	default.get_hotbar_bg(0,5.75)
