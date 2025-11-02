4e061d905d2ff0494a295b6134ff63780bcf5a50
Luke aka SwissalpS <161979+SwissalpS@users.noreply.github.com>
2025-11-02 07:49:16 +0100

    whitespace fix

--- nodes/node_diggers.lua
+++ nodes/node_diggers.lua
@@ -454,7 +454,7 @@ minetest.register_node("digtron:dual_digger", {
 			end
 			cost = cost + forward_cost
 		end
-			
+
 		if nodes_dug:get_pos(digdown) then
 			return cost, items
 		end
@@ -546,7 +546,7 @@ minetest.register_node("digtron:dual_soft_digger", {
 			end
 			cost = cost + forward_cost
 		end
-			
+	
 		if nodes_dug:get_pos(digdown) then
 			return cost, items
 		end
